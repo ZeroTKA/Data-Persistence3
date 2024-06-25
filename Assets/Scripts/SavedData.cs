46a9fe21bdb723758e6fb9d833fe328b188bd12c
ZeroTKA <zerotka@gmail.com>
2024-06-25 15:50:54 -0600

    SavedData Persists
    The gameObject persists between loads.

--- Assets/Scripts/SavedData.cs
+++ Assets/Scripts/SavedData.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class SavedData : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static SavedData Instance;
+    public string DisplayName;
+    private void Awake()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(Instance);
     }
 }
